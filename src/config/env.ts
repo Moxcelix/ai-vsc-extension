1654d1821851e5e6a639fdc769dd2907d22e5d56
moxcelix <maxim.spf@gmail.com>
2025-09-29 13:36:57 +0300

    gigachat llm implemented

---
+++ src/config/env.ts
@@ -0,0 +1,8 @@
+export class Env {
+    public GigachatClientID :string = '01996760-13d1-7f11-8890-a26c15069f42';
+    public GigachatClientSecret: string = '6c981378-d8ff-43db-8045-639ff27a24e9';
+    public GigachatScope: string = 'GIGACHAT_API_PERS';
+    public GigachatApiUrl: string ='https://gigachat.devices.sberbank.ru/api/v1';
+    public GigachatTokenUrl:string = 'https://ngw.devices.sberbank.ru:9443/api/v2/oauth';
+
+}

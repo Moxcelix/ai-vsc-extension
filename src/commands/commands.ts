af84705bf1e728b198e6d0d964ae60018cbc85ec
moxcelix <maxim.spf@gmail.com>
2025-09-30 14:11:32 +0300

    ai file editing is implemented

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,17 +1,23 @@
+import { FileEditController } from "./controllers/file_edit";
 import { QuestionController } from "./controllers/question"
 import * as vscode from 'vscode';
 
 
 export class Commands {
     private questionController: QuestionController;
+    private fileEditController: FileEditController;
 
-    public constructor(questionCommand: QuestionController){
+    public constructor(
+        questionCommand: QuestionController, 
+        fileEditController: FileEditController){
         this.questionController = questionCommand;
+        this.fileEditController = fileEditController;
     }
 
     public setup(context: vscode.ExtensionContext){
         context.subscriptions.push(
             vscode.commands.registerCommand('ai-assistant.askQuestion', this.questionController.command),
+            vscode.commands.registerCommand('ai-assistant.editFile', this.fileEditController.command),
         );
     }
 }
